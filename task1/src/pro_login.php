7118282e7d4b2383274e751ebfe6e894ef357833
Cao Dang Chuong <92881216+caodchuong312@users.noreply.github.com>
2023-02-10 23:10:29 +0700

    Update pro_login.php

--- task1/src/pro_login.php
+++ task1/src/pro_login.php
@@ -5,14 +5,14 @@
 	$password=$_POST['password'];
 	$password=md5($password);
 	$sql="select * from user where username='$username' and password='$password'";
-	$result=mysqli_query($conn,$sql);
-	$user = mysqli_fetch_array($result);
-	$id = $user['id'];
-	if (mysqli_num_rows($result)>0) {
+	$result=mysqli_query($conn,$sql);	
+	if (mysqli_num_rows($result)==0) {
+		die("Thông tin không chính xác");	
+	}else{
+		$user = mysqli_fetch_array($result);
+		$id = $user['id'];
 		echo "Đăng nhập thành công";
 		$_SESSION['user']= $username;	
 		$_SESSION['id']= $id;
 		header("location:index.php");
-	}else{
-		echo "Thông tin không chính xác";
 	}
