f55f13b95a6ae93a0b5a5e8bfdd1f569cc208a02
Cao Dang Chuong <92881216+caodchuong312@users.noreply.github.com>
2023-02-24 16:23:14 +0700

    Update index.php

--- task3/error_based_sqli/index.php
+++ task3/error_based_sqli/index.php
@@ -51,36 +51,24 @@
 </html>
 <?php 
 require "config.php";
-// if($conn){
-// 	echo "connect successfully";
-// }
 if(isset($_GET['type'])){
-	preg_match('/union|and|or|if|case|count|sleep|\=/i', $_GET['type']);
 	$type=$_GET['type'];
-	// $type = $_GET['type'];
-	echo "<br>";
-	echo "<br>";
-	echo "selected: ".$type;
 	echo "<br>";
 	echo "<br>";
 
-	$sql="SELECT * from products where type= '$type' ";
+	$sql="SELECT * from products where type= '$type'";
 	$result = mysqli_query($conn,$sql);
-	if (!$result) {
-    	 echo "Lỗi trong truy vấn: " . mysqli_error($conn);
-	}
-	else{
-		echo '<table>';
-		echo '<tr><th>Name</th><th>Price</th><th></th></tr>';
-		while ($row = mysqli_fetch_array($result)) {
-		  echo '<tr>';
-		  echo '<td>' . $row['name'] . '</td>';
-		  echo '<td>' . $row['price'] . '</td>';
-		  echo '<td><a class="detail" href="">Xem chi tiết</a></td>';
-		  echo '</tr>';
-		}
-		echo '</table>';
-	}
-	
-
+    	print_r(mysqli_error($conn));
+    	if(mysqli_fetch_assoc($result)){
+			echo '<table>';
+			echo '<tr><th>Name</th><th>Price</th><th></th></tr>';
+			while ($row = mysqli_fetch_assoc($result)) {
+			echo '<tr>';
+			echo '<td>' . $row['name'] . '</td>';
+			echo '<td>' . $row['price'] . '</td>';
+			echo '<td><a class="detail" href="">Xem chi tiết</a></td>';
+			echo '</tr>';
+			}
+			echo '</table>';			
+    	}		
 }
